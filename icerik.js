af21723b5b93533f32393b0fa4d31d7c49d827c2
dusunselikitapkulubu <dusunselikitapkulubu@gmail.com>
2026-03-10 13:10:39 +0300

    Update icerik.js

--- icerik.js
+++ icerik.js
@@ -29,9 +29,7 @@ const ICERIK = {
       {
         emoji:    "🟡",
         ad:       "Lobi",
-        aciklama: "Lobi Üyeleri, başvuru formunu doldurmuş, başvurusu onaylanmış fakat Salon Üyesi olma şartını henüz sağlayamamış katılımcılardan ve katılım şartını sağlamadığı için mevcut Salon Üyeliği, Lobi Üyeliğine düşürülmüş katılımcılardan oluşur. Başka bir deyişle, kulübe katılan her yeni üye Lobi grubundan kulübe giriş yapar. Katılım durumuna göre Salon grubuna geçebilir.
-          Lobide kalma süresi azami 3 aydır. Lobiye alındıktan itibaren 3 ay içerisinde herhangi bir okumaya katılmayan üye lobiden çıkarılır.
-          Kendi isteğiyle kulüpten ayrılan üye veya kulüpten devamsızlık sebebiyle çıkarılan üye, tekrar kulübe katılmak isterse, kulüp kurallarında belirtilen tüm başvuru aşamalarını yeniden tamamlamak durumundadır.",
+        aciklama: "Lobi Üyeleri, başvuru formunu doldurmuş, başvurusu onaylanmış fakat Salon Üyesi olma şartını henüz sağlayamamış katılımcılardan ve katılım şartını sağlamadığı için mevcut Salon Üyeliği, Lobi Üyeliğine düşürülmüş katılımcılardan oluşur. Başka bir deyişle, kulübe katılan her yeni üye Lobi grubundan kulübe giriş yapar. Katılım durumuna göre Salon grubuna geçebilir.Lobide kalma süresi azami 3 aydır. Lobiye alındıktan itibaren 3 ay içerisinde herhangi bir okumaya katılmayan üye lobiden çıkarılır. Kendi isteğiyle kulüpten ayrılan üye veya kulüpten devamsızlık sebebiyle çıkarılan üye, tekrar kulübe katılmak isterse, kulüp kurallarında belirtilen tüm başvuru aşamalarını yeniden tamamlamak durumundadır.",
       },
       {
         emoji:    "🟢",
